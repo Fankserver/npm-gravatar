5b5a86a02df69230cc69abcd34e97ab14a82595e
Florian Kinder <Florian.Kinder@fankserver.com>
2016-02-13 00:26:46 +0100

    Removed obsolete namespace

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,14 +1,12 @@
-export function url (email: string, options?: gravatar.Options, addProtocol?: boolean): string;
+export function url (email: string, options?: Options, addProtocol?: boolean): string;
 
-declare namespace gravatar {
-  export interface Options {
-    s?: number | string;
-    size?: number | string;
-    r?: string;
-    rating?: string;
-    d?: string;
-    default?: string;
-    f?: string;
-    forcedefault?: string;
-  }
+export interface Options {
+  s?: number | string;
+  size?: number | string;
+  r?: string;
+  rating?: string;
+  d?: string;
+  default?: string;
+  f?: string;
+  forcedefault?: string;
 }
